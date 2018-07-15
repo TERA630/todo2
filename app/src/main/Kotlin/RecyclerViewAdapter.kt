8510f4cbaaf68e3bfc1c3e317552ab9b7cfeb917
TERA630 <tera.bcool.4475@gmail.com>
2018-07-15 13:50:06 +0900

    Navigation was made

--- app/src/main/Kotlin/RecyclerViewAdapter.kt
+++ app/src/main/Kotlin/RecyclerViewAdapter.kt
@@ -8,7 +8,9 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.list_items.view.*
+import org.koin.android.ext.android.inject
 
 /*
     MVC/MVP - View
@@ -16,18 +18,22 @@ import kotlinx.android.synthetic.main.list_items.view.*
     データ変更を伴うユーザーの入力イベントは　Controller/Presenterに委譲する方針で｡
  */
 
-class RecyclerViewAdapter(private val mList:List<String>)
+class RecyclerViewAdapter(private val mList: List<String>, private val viewModel: MainViewModel)
     : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+
     // method
     override fun getItemCount(): Int = mList.size
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         holder.itemView.itemTile.text = mList[position]
     }
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder{
         val rowView = LayoutInflater.from(parent.context).inflate(R.layout.list_items, parent,false)
-        Log.i("test","onCreateViewHolder")
+        val option = viewModel.navOptions
+        rowView.editBtn.setOnClickListener { v: View ->
+            val navController = Navigation.findNavController(v)
+            navController.navigate(R.id.fragment_detail, null, option)
+        }
         return RecyclerViewHolder(rowView)
     }
 
