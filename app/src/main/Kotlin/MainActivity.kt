17126917200b41ce03a3db56c82d22b474cb52b6
Yoshihiko <tera.bcool.4475@gmail.com>
2018-08-06 16:40:50 +0900

    fix itemlist

--- app/src/main/Kotlin/MainActivity.kt
+++ app/src/main/Kotlin/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.yoshi.todo2
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import org.koin.android.architecture.ext.getViewModel
 import org.koin.android.ext.android.inject
 
@@ -20,6 +23,19 @@ class MainActivity : AppCompatActivity() {
         super.onPause()
         val repository = Repository()
         repository.saveListToPreference(vModel.getItemList(), this.applicationContext)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_makedefault -> Log.i("test", "Item default Selected.")
+            R.id.menu_help -> Log.i("test", "Help selected.")
+        }
+        return true
     }
 }
 
